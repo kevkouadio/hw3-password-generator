f1205e68d0392ab5684915ec3dfafc397218fc84
Kouassi Kevin Kouadio <kevkouadio>
2020-07-22 12:44:21 -0400

    correction after grader comment

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Alphabetical characters
 lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "w", "X", "Y", "Z"];
+upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //User Choices variable
 var choices;
@@ -71,7 +71,7 @@ function generatePassword() {
         choices = lowerCases.concat(upperCases);
 
     } else if (confirmNumber && confirmUppercases) {
-        choices = number.concat(lowerCases);
+        choices = number.concat(upperCases);
     }
     // Else if for 1 positive choice
     else if (confirmCharacters) {
